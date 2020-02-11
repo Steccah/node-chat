c75c16f7a872a1ce3ade7ffb0a0d6616f9d12429
McStecca <andreastecca2001@gmail.com>
2020-02-11 23:36:02 +0100

    First upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+//document.getElementById('txt').remove();
+
+//variables
+var onlinelist = [];
+
+//express
+app.use(express.static(__dirname + '/views'))
+
+app.get('/', function(req, res) {
+    res.render('chat.ejs');
+});
+
+//socket
+io.sockets.on('connection', function(socket) {
+
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+        io.emit('onlinelist', onlinelist);
+        onlinelist.push(socket.username);
+    });
+
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+        //elimina chi non è più online
+        for (var i = 0; i < onlinelist.length; i++) {
+            if (onlinelist[i] == socket.username){
+                onlinelist.splice(i, 1);
+                break;
+            }
+        }
+        io.emit('onlinelist', onlinelist)
+    });
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+    socket.on('is_typing', function(username){
+        io.emit('ext_typing', socket.username + ' is typing');
+    });
+
+});
+
+//server
+const server = http.listen(8080, function() {
+    console.log('listening on http://localhost:8080');
+});
